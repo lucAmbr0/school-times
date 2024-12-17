ee23239bf3902df829150583199515fc89bc2fce
Luca <luca.ambrosonee@gmail.com>
2024-12-17 17:20:53 +0100

    Added vercel speedinsights

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <SpeedInsights />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -29,7 +30,6 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <SpeedInsights />
     </>
   )
 }
