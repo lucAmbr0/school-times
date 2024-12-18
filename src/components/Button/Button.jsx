2cc752d022207d6a716fd6b6e42c9ae17de661a5
Luca <luca.ambrosonee@gmail.com>
2024-12-18 22:03:33 +0100

    Added colors, fonts, button component with variants

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import 'material-symbols';
+import styles from './Button.module.css';
+
+// Ex. usage:
+// <Button text="Done" variant="filled" iconName="check" onClick={() => {console.log("Clicked!")}} />
+
+const errNoClkEvent = () => { console.error("Error: No onClick event provided") };
+
+function Button({ text = "", variant = "filled", iconName = "", onClick = errNoClkEvent }) {
+  const btnClasses = `${styles.button} ${styles[variant]} ${!text && styles.iconOnly}`
+  const iconClasses = `material-symbols-outlined ${styles.icon}`;
+  
+  return (
+    <button className={btnClasses} onClick={onClick}>
+      {iconName && <span className={iconClasses}>{iconName}</span>}
+      {text && text}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  text: PropTypes.string,
+  type: PropTypes.string,
+  iconName: PropTypes.string,
+  onClick: PropTypes.func,
+};
+
+export default Button;
