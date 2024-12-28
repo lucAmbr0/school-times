d12f1c366019564a48ed663013707b2e4de679ae
Luca <luca.ambrosonee@gmail.com>
2024-12-28 16:29:26 +0100

    Added service worker

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'vite-pwa-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.webmanifest',
+  '/favicon.svg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => 
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) =>
+      cachedResponse || fetch(event.request)
+    )
+  );
+});
