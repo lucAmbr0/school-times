7337fb32bdd3f95e72f557dbe664983e155ae8d6
Luca <luca.ambrosonee@gmail.com>
2025-03-22 17:37:21 +0100

    Added title component to Home tab

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import ButtonTable from '../../components/Table/ButtonTable';
+import styles from './Home.module.css';
 
 function Home() {
 
   const element = (
     <>
+    <h1 className={styles.title}>Welcome, Guest - 0Z</h1>
     <ButtonTable />
   </>
   )
