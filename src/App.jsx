5b897f39eefd7959fdd969a27959722a1c344a50
Luca <luca.ambrosonee@gmail.com>
2025-03-23 18:14:16 +0100

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import React, { useState } from "react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import { Analytics } from "@vercel/analytics/react"
+import { track } from "@vercel/analytics";
 import useThemeColor from "./scripts/useThemeColor";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
--- src/components/Navbar/NavbarBtn/NavbarBtn.jsx
+++ src/components/Navbar/NavbarBtn/NavbarBtn.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./NavbarBtn.module.css";
+import {track} from "@vercel/analytics";
 
-function NavbarBtn({onNavigate, text = "", iconName = "", active = false}) {
+function NavbarBtn({ onNavigate, text = "", iconName = "", active = false }) {
   const iconClasses = `material-symbols-outlined ${styles.icon}`;
   const pillClasses = `${styles.pill} ${active && styles.pillActive}`;
   const textClasses = `${styles.label} ${active && styles.labelActive}`;
 
   const element = (
-    <button onClick={() => onNavigate(text)}>
+    <button
+      onClick={() => {
+        onNavigate(text);
+        track('Navbar Click', { buttonText: text }, { flags: ['navbar-action'] });
+      }}
+    >
       <div className={pillClasses}>
         {iconName && <span className={iconClasses}>{iconName}</span>}
       </div>
