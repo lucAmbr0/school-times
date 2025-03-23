74391f359f4f700825d710304de0f5a2b4aa9c58
Luca <luca.ambrosonee@gmail.com>
2025-03-23 23:17:06 +0100

    Added Swiper component (experimental)

---
+++ src/components/SwiperWrap/SwiperWrap.jsx
@@ -0,0 +1,76 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import styles from "./SwiperWrap.module.css";
+import { useEffect } from "react";
+
+function SwiperWrap({ type, start, length }) {
+  const onSlideChange = (swiper) => {
+    const centeredIndex = swiper.activeIndex;
+    swiper.slides.forEach((slide) => {
+      slide.classList.remove(styles.selectedSlide);
+    });
+    const centeredSlide = swiper.slides[centeredIndex];
+    console.log(centeredSlide ? centeredSlide.innerText : "No centered slide");
+    setTimeout(() => {
+      swiper.slides.forEach((slide) => {
+        if (slide.innerText == centeredSlide.innerText) {
+          slide.classList.add(styles.selectedSlide);
+          swiper.update();
+        }
+      });
+    }, 5);
+  };
+
+  
+  const generateSlides = () => {
+    const slides = [];
+    if (type == "time") {
+      for (let i = 0; i < length; i++) {
+        const hour = start + i;
+        slides.push(
+          <SwiperSlide
+            key={`time-${hour}`}
+            className={[styles.swiperSlide, (i == 0 ? styles.selectedSlide : "")].join(" ")}
+          >{`${hour}:00`}</SwiperSlide>
+        );
+      }
+    } else if (type == "days") {
+      const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+      for (let i = 0; i < days.length; i++) {
+        slides.push(
+          <SwiperSlide
+            key={`day-${days[i]}`}
+            className={[styles.swiperSlide, (i == 0 ? styles.selectedSlide : "")].join(" ")}
+          >{`${days[i]}`}</SwiperSlide>
+        );
+      }
+    }
+    return slides;
+  };
+
+  const slides = generateSlides();
+  
+  const element = (
+    <>
+      <Swiper
+        a11y={true}
+        slidesPerView={5}
+        centeredSlides={true}
+        onSlideChange={onSlideChange}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={0}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        className={styles.swiper}
+        >
+        {slides}
+      </Swiper>
+    </>
+  );
+  
+  return element;
+}
+
+export default SwiperWrap;
--- src/pages/Schedule/Schedule.jsx
+++ src/pages/Schedule/Schedule.jsx
@@ -1,17 +1,18 @@
-function Schedule() {
+import SwiperWrap from "../../components/SwiperWrap/SwiperWrap";
+import styles from "./Schedule.module.css";
 
+function Schedule() {
   const element = (
     <>
-    <div>
-      <h1>Schedule</h1>
-      <h2>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse laborum nobis aperiam ducimus odio tempore molestias deserunt unde odit inventore! Eius dignissimos provident distinctio consequuntur quia dolore voluptates pariatur minus.</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos, rem maxime excepturi veniam quis fugiat officia ab quasi natus quaerat aliquid repellat molestias. Consequatur doloremque earum mollitia doloribus quos beatae?</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium magni porro adipisci odio ipsum! Perferendis accusantium non ea, nisi nam ullam similique quo vero hic minus soluta adipisci nobis provident.</p>
+    <div className={styles.swipers}>
+    <SwiperWrap className={styles.swiper} type={"days"} />
+    <SwiperWrap className={styles.swiper} type={"time"} start={7} length={10} />
+
     </div>
-  </>
-  )
-  
-  return ( element );
+    </>
+  );
+
+  return element;
 }
 
 export default Schedule;
