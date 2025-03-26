392bd3e58b438e4c309f871d0915d421c3d37942
Luca <luca.ambrosonee@gmail.com>
2025-03-26 09:38:11 +0100

    fixed start slide class

--- src/components/SwiperWrap/SwiperWrap.jsx
+++ src/components/SwiperWrap/SwiperWrap.jsx
@@ -8,13 +8,14 @@ import "swiper/css/pagination";
 import { useState, useEffect } from "react";
 
 function SwiperWrap({ type, start, length }) {
-  const [activeSlide, setActiveSlide] = useState(0);
-  
+  const [activeSlide, setActiveSlide] = useState(Infinity);
+
   useEffect(() => {
     if (type === "time") {
-      setActiveSlide(new Date().getHours() - 7);
+      // setActiveSlide(new Date().getHours() - 7);
+      setActiveSlide(0);
     } else if (type === "days") {
-      setActiveSlide(new Date().getDay() - 1);
+      setActiveSlide(6);
     }
   }, []);
 
@@ -23,7 +24,9 @@ function SwiperWrap({ type, start, length }) {
     if (type === "time") {
       for (let i = 0; i < length; i++) {
         const hour = start + i;
-        const slst = `${styles.swiperSlide} ${i === activeSlide ? styles.selectedSlide : ""}`
+        const slst = `${styles.swiperSlide} ${
+          i === activeSlide ? styles.selectedSlide : ""
+        }`;
         slides.push(
           <SwiperSlide
             key={`time-${hour}`}
@@ -34,7 +37,9 @@ function SwiperWrap({ type, start, length }) {
     } else if (type === "days") {
       const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
       for (let i = 0; i < days.length; i++) {
-        const slst = `${styles.swiperSlide} ${i === activeSlide ? styles.selectedSlide : ""}` 
+        const slst = `${styles.swiperSlide} ${
+          i === activeSlide ? styles.selectedSlide : ""
+        }`;
         slides.push(
           <SwiperSlide
             key={`day-${days[i]}`}
@@ -47,59 +52,57 @@ function SwiperWrap({ type, start, length }) {
   };
 
   const slides = generateSlides();
-
-  const element = (
-    <>
-      <Swiper
-        slidesPerView={5}
-        centeredSlides={true}
-        modules={[FreeMode, Pagination, Navigation]}
-        spaceBetween={0}
-        className={styles.swiper}
-        touchRatio={1}
-        resistanceRatio={0.85}
-        longSwipes={true}
-        longSwipesRatio={0.1}
-        longSwipesMs={300}
-        initialSlide={activeSlide}
-        slideActiveClass={styles.selectedSlide}
-        slideToClickedSlide={true}
-        grabCursor={true}
-        cssMode={false}
-        freeMode={{
-          enabled: true,
-          momentum: true,
-          momentumRatio: 0.2,
-          momentumVelocityRatio: 0.2,
-          sticky: true,
-        }}
-        onSlideChange ={(swiper) => {
-          if (swiper.params.slideActiveClass) {
-            swiper.params.slideActiveClass = null;
-          }
-        }}
-        onSlideChangeTransitionStart={(swiper) => {
-          swiper.slides.forEach(s => {
-            s.classList.remove(`${styles.selectedSlide}`);
-          })
-        }}
-        onSlideChangeTransitionEnd={(swiper) => {
-          setActiveSlide(swiper.activeIndex);
-          handleSlideChange(swiper, swiper.slides[swiper.activeIndex]);
-        }}
-      >
-        {slides}
-      </Swiper>
-    </>
+  if (activeSlide == Infinity) {
+    return null;
+  }
+  return (
+    <Swiper
+      slidesPerView={5}
+      centeredSlides={true}
+      modules={[FreeMode, Pagination, Navigation]}
+      spaceBetween={0}
+      className={styles.swiper}
+      touchRatio={1}
+      resistanceRatio={0.85}
+      longSwipes={true}
+      longSwipesRatio={0.1}
+      longSwipesMs={300}
+      initialSlide={activeSlide}
+      slideActiveClass={styles.selectedSlide}
+      slideToClickedSlide={true}
+      grabCursor={true}
+      cssMode={false}
+      freeMode={{
+        enabled: true,
+        momentum: true,
+        momentumRatio: 0.2,
+        momentumVelocityRatio: 0.2,
+        sticky: true,
+      }}
+      onSlideChange={(swiper) => {
+        if (swiper.params.slideActiveClass) {
+          swiper.params.slideActiveClass = null;
+        }
+      }}
+      onSlideChangeTransitionStart={(swiper) => {
+        swiper.slides.forEach((s) => {
+          s.classList.remove(`${styles.selectedSlide}`);
+        });
+      }}
+      onSlideChangeTransitionEnd={(swiper) => {
+        setActiveSlide(swiper.activeIndex);
+        handleSlideChange(swiper, swiper.slides[swiper.activeIndex]);
+      }}
+    >
+      {slides}
+    </Swiper>
   );
-
-  return element;
 }
 
 function handleSlideChange(swiper, activeSlide) {
-  swiper.slides.forEach(s => {
+  swiper.slides.forEach((s) => {
     s.classList.remove(`${styles.selectedSlide}`);
-  })
+  });
   activeSlide.classList.add(styles.selectedSlide);
 }
 
