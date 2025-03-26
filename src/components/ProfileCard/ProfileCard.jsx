3e3756bc11f32e8ab5c13219b415b7cd309a02ec
Luca <luca.ambrosonee@gmail.com>
2025-03-26 22:21:15 +0100

    minor changes

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -3,6 +3,7 @@ import ProfileOverview from "./ProfileOverview/ProfileOverview";
 import FavoriteSubject from "./FavoriteSubject/FavoriteSubject";
 import StudentStats from "./StudentStats/StudentStats";
 import GradesList from "./GradesList/GradesList";
+import HorizontalLine from "../Separator/HorizontalLine";
 import { useData } from "../../scripts/useData";
 
 function ProfileCard() {
@@ -12,13 +13,21 @@ function ProfileCard() {
     <div className={styles.container}>
       <ProfileOverview user={data.user} />
       <FavoriteSubject favSubject={data.user.favoriteSubject} />
+      <HorizontalLine
+        length={"90%"}
+        height={"1px"}
+        margin={"5px auto"}
+        color={"var(--palette-300)"}
+        alpha={0.75}
+      />
+      <h3 className={styles.sectionLabel}>Year overview</h3>
       <div className={styles.statsContainer}>
         <StudentStats label={"Absences"} value={0} />
         <StudentStats label={"Infractions"} value={0} />
         <StudentStats label={"Delays"} value={0} />
         <StudentStats label={"GPA"} value={0} />
       </div>
-      <h3 className={styles.latestGradesLabel}>Latest grades</h3>
+      <h3 className={styles.sectionLabel}>Latest grades</h3>
       <GradesList grades={["", "", "", "", "", "", "", ""]} />
     </div>
   );
