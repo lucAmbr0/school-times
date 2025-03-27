933367587a9252c8ffd3c1fe8c7f58c5dc5191f9
Luca <luca.ambrosonee@gmail.com>
2025-03-27 17:09:47 +0100

    Added border-radius customization to Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,13 +4,13 @@ import 'material-symbols';
 import styles from './Button.module.css';
 
 // Ex. usage:
-// <Button text="Done" variant="filled" iconName="check" onClick={() => {console.log("Clicked!")}} />
+// <Button text="Done" border="square" variant="filled" iconName="check" onClick={() => {console.log("Clicked!")}} />
 
 const errNoClkEvent = () => { console.error("Error: No onClick event provided") };
 
-function Button({ text = "", variant = "filled", iconName = "", onClick = errNoClkEvent }) {
-  const btnClasses = `${styles.button} ${styles[variant]} ${!text && styles.iconOnly}`
+function Button({ text = "", border = "soft", variant = "filled", iconName = "", onClick = errNoClkEvent }) {
   const iconClasses = `material-symbols-outlined ${styles.icon}`;
+  let btnClasses = `${styles.button} ${styles[border]} ${styles[variant]} ${!text && styles.iconOnly}`
   
   return (
     <button className={btnClasses} onClick={onClick}>
