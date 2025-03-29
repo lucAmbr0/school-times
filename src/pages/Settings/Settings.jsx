03ba558d083dbd87148215e9cd9ed187a6b32a12
Luca <luca.ambrosonee@gmail.com>
2025-03-29 17:06:20 +0100

    Added structure for settings

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,3 +1,4 @@
+import Button from "../../components/Button/Button";
 import styles from "./Settings.module.css";
 
 function Settings({onBack}) {
@@ -8,6 +9,12 @@ function Settings({onBack}) {
           <button onClick={onBack}>
             <span className="material-symbols-outlined">arrow_back</span>Back
           </button>
+        </div>
+        <h1 className={styles.title}>Settings</h1>
+        <div className={styles.settingsSection}>
+          <h3 className={styles.sectionTitle}>Preferences</h3>
+          <div className={styles.settingsGrid}>
+          </div>
         </div>
       </div>
     </>
