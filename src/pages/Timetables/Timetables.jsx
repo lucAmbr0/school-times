faa161f9a216ed9f173505459d356bac93430e0b
Luca <luca.ambrosonee@gmail.com>
2025-04-04 17:05:45 +0200

    minor changes

--- src/pages/Timetables/Timetables.jsx
+++ src/pages/Timetables/Timetables.jsx
@@ -1,6 +1,7 @@
 import PageHeader from "../../components/PageHeader/PageHeader";
 import styles from "./Timetables.module.css";
 import useVibration from "../../scripts/useVibration";
+// import Dropdown from "../../components/Dropdown/ThemeSelector";
 
 function Timetables({onBack}) {
   const vibrate = useVibration();
@@ -19,6 +20,15 @@ function Timetables({onBack}) {
       <PageHeader handleBack={handleBack} />
       <div id="timetablesPage" className={styles.container}>
         <h1 className={styles.title}>Timetables</h1>
+        <div className={styles.timetableSettingsContainer}>
+            <div className={styles.classSelectorContainer}>
+                <p>Class</p>
+                {/* <Dropdown /> */}
+            </div>
+            <div className={styles.classInfoContainer}>
+
+            </div>
+        </div>
       </div>
     </>
   );
