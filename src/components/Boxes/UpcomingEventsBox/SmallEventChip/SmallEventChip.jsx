8628ad614858e5e69beef58aa72120a4f0778b52
Luca <luca.ambrosonee@gmail.com>
2025-04-07 13:27:21 +0200

    Added events from Events tab to UpcomingEventsBox component in Home tab

--- src/components/Boxes/UpcomingEventsBox/SmallEventChip/SmallEventChip.jsx
+++ src/components/Boxes/UpcomingEventsBox/SmallEventChip/SmallEventChip.jsx
@@ -2,10 +2,14 @@ import styles from "./SmallEventChip.module.css";
 
 function SmallEventChip({day = "00", eventName = "No Event"}) {
 
+  const extractDay = (date) => {
+    return date.split(", ")[1].split(" ")[0];
+  };
+  
   return (
     <div className={styles.chipContainer}>
-        <div className={styles.dayContainer}>{day}</div>
-        <div className={styles.dayContainerInv}>{day}</div>
+        <div className={styles.dayContainer}>{extractDay(day)}</div>
+        <div className={styles.dayContainerInv}>{extractDay(day)}</div>
         <h3 className={styles.eventLabel}>{eventName}</h3>
     </div>
   );
--- src/components/Boxes/UpcomingEventsBox/UpcomingEventsBox.jsx
+++ src/components/Boxes/UpcomingEventsBox/UpcomingEventsBox.jsx
@@ -1,16 +1,31 @@
 import boxStyles from "../Box.module.css";
 import SmallEventChip from "./SmallEventChip/SmallEventChip";
+import { samplePosts } from "../../../scripts/events";
 import styles from "./UpcomingEventsBox.module.css";
 
-function UpcomingEventsBox({}) {
+function UpcomingEventsBox() {
 
+  const generateEvents = () => {
+    let events = [];
+    let i = 0;
+    for (let upcEvent of samplePosts) {
+      events.push(
+        <SmallEventChip day={upcEvent.date} eventName={upcEvent.text} key={"upcEvent-" + i} />
+      )
+      i++;
+      if (i >= 5) break;
+    }
+    return events;
+  }
+
+  const events = generateEvents();
+  const noEventsElement = <h3 className={styles.noEventsLabel}>No upcoming events</h3>
+  
   return (
     <div className={boxStyles.box}>
         <h3 className={boxStyles.boxTitle}>Upcoming</h3>
         <div className={styles.eventsContainer}>
-            <SmallEventChip />
-            <SmallEventChip />
-            <SmallEventChip />
+          {events.length > 0 ? events : noEventsElement}
         </div>
     </div>
   );
