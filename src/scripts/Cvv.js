1835a058df7aeddbe2ed9d0c0de5eeea9e7e700e
Luca <luca.ambrosonee@gmail.com>
2025-04-14 20:44:11 +0200

    Added cvv api functions

---
+++ src/scripts/Cvv.js
@@ -0,0 +1,140 @@
+export class Cvv {
+
+    static API_URL = 'https://cloudflare-cors-anywhere-1.vercel.app/?https://web.spaggiari.eu/rest/v1';
+    static API_KEY = import.meta.env.VITE_CVV_Z_DEV_API_KEY;
+    static sessionToken = "";
+
+    static async getToken(username, password) {
+        try {
+            const response = await fetch(`${Cvv.API_URL}/auth/login`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'CVVS/std/4.1.7 Android/10',
+                    'Z-Dev-Apikey': this.API_KEY,
+                },
+                body: JSON.stringify({
+                    'uid': username,
+                    'pass': password
+                })
+            });
+            const data = await response.json();
+            console.log('GET Response:', data);
+            return data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            return data;
+        }
+    }
+
+    static async getGrades() {
+        try {
+            const response = await fetch(`${Cvv.API_URL}/students/${studentId}/grades/?ffilter=grades(displayValue)`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'CVVS/std/4.1.7 Android/10',
+                    'Z-Dev-Apikey': this.API_KEY,
+                    'Z-Auth-Token': sessionToken
+                },
+            });
+            const data = await response.json();
+            console.log('GET Response:', data);
+            return data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            return data;
+        }
+    }
+
+    static async getAbsences() {
+        try {
+            // ABA0 = full (red) day absence
+            const response = await fetch(`${Cvv.API_URL}/students/${studentId}/absences/details/?dfilter=events(evtCode=ABA0)`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'CVVS/std/4.1.7 Android/10',
+                    'Z-Dev-Apikey': this.API_KEY,
+                    'Z-Auth-Token': sessionToken
+                },
+            });
+            const data = await response.json();
+            console.log('GET Response:', data);
+            return data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            return data;
+        }
+    }
+
+    static async getDelays() {
+        try {
+            // ABR0 = delay (orange)
+            const response = await fetch(`${Cvv.API_URL}/students/${studentId}/absences/details/?dfilter=events(evtCode=ABR0)`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'CVVS/std/4.1.7 Android/10',
+                    'Z-Dev-Apikey': this.API_KEY,
+                    'Z-Auth-Token': sessionToken
+                },
+            });
+            const data = await response.json();
+            console.log('GET Response:', data);
+            return data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            return data;
+        }
+    }
+
+    static async getDelays() {
+        try {
+            // NTNT: appunti
+            // NTTE: note dell'insegnante
+            // NTCL: note sul registro
+            // NTWN: richiami
+            // NTST: sanzioni disciplinari
+            const response = await fetch(`${Cvv.API_URL}/students/${studentId}/notes/all/`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'CVVS/std/4.1.7 Android/10',
+                    'Z-Dev-Apikey': this.API_KEY,
+                    'Z-Auth-Token': sessionToken
+                },
+            });
+            const data = await response.json();
+            console.log('GET Response:', data);
+            return data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            return data;
+        }
+    }
+
+    static async getStatus() {
+        try {
+            // HAT0: presente a lezione
+            // HAT1: presente fuori aula
+            // HAB0: assente a lezione
+            // HNN0: ora senza lezione
+            const response = await fetch(`${Cvv.API_URL}/students/${studentId}/lessons-status/today`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'CVVS/std/4.1.7 Android/10',
+                    'Z-Dev-Apikey': this.API_KEY,
+                    'Z-Auth-Token': sessionToken
+                },
+            });
+            const data = await response.json();
+            console.log('GET Response:', data);
+            return data;
+        } catch (error) {
+            console.error('GET Error:', error);
+            return data;
+        }
+    }
+}
